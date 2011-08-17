06055b862815bab34c50ed987b16f26e5105fcdb
johnny0625 <johnny0625@hotmail.com>
2011-08-17 11:36:03 +0000

    Add Project

---
+++ GUI/WpfApplication1/Controls/Menu/NewGame/NewGameControl2.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1.Controls.Menu.NewGame
+{
+    /// <summary>
+    /// Interaction logic for NewGameControl2.xaml
+    /// </summary>
+    public partial class NewGameControl2 : UserControl
+    {
+        public NewGameControl2()
+        {
+            InitializeComponent();
+
+            Screen1Control.Content = new NewGameControl2Control();
+            Screen2Control.Content = new NewGameControl2Control();
+        }
+
+        private void AddScreenButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ GUI/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApplication1.Controls.Menu;
+using WpfApplication1.Controls.Menu.NewGame;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+        }
+
+        private void NewGameMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            var newGame = new NewGameWindow() { Owner = this };
+            newGame.ShowDialog();
+        }
+
+        private void ExitMenuItemClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+
+    }
+}
